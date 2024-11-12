6ee7cb28cdc9355a50a3fd3babbcc26ea0c83739
goplayzig <goplayzig@gmail.com>
2024-11-12 19:11:00 +0900

    add waiting logic

--- userprog/process.c
+++ userprog/process.c
@@ -208,7 +208,7 @@ process_exec (void *f_name) {
 	palloc_free_page (file_name);
 	if (!success)
 		return -1;
-
+	hex_dump(_if.rsp, _if.rsp, KERN_BASE - _if.rsp, true);
 	/* Start switched process. */
 	do_iret (&_if);
 	NOT_REACHED ();
@@ -266,6 +266,7 @@ process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
+	while (1) {}
 	return -1;
 }
 
