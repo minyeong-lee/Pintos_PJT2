223d910088a888ce5f37f676e31e3978e3c411b2
goplayzig <goplayzig@gmail.com>
2024-11-12 17:06:23 +0900

    Test 1

--- userprog/process.c
+++ userprog/process.c
@@ -50,6 +50,11 @@ process_create_initd (const char *file_name) {
 		return TID_ERROR;
 	strlcpy (fn_copy, file_name, PGSIZE);
 
+	// Argument Passing ~ 
+	char *save_ptr;
+	strtok_r(file_name, " ", &save_ptr);
+	// ~ Argument Passing
+
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
 	if (tid == TID_ERROR)
@@ -163,7 +168,6 @@ error:
 int
 process_exec (void *f_name) {
 	char *file_name = f_name;
-	printf("파일 이름 : %s", file_name);
 	bool success;
 
 	/* We cannot use the intr_frame in the thread structure.
@@ -177,8 +181,28 @@ process_exec (void *f_name) {
 	/* We first kill the current context */
 	process_cleanup ();
 
+	// Argument Passing ~ 
+	char *parse[64];
+	char *token, *save_ptr;
+	int count = 0;
+
+	token = strok_r(file_name, " ", &save_ptr);
+
+	while (token != NULL) {
+		parse[count++] = token;
+		token = strtok_r(NULL, " ", &save_ptr);
+	}
+	
+
+
 	/* And then load the binary */
-	success = load (file_name, &_if);
+	success = load(file_name, &_if);
+
+	//파싱된 값을 스택에 넘겨는 코드 연결 
+	// Argument Passing ~
+    argument_stack(parse, count, &_if.rsp); // 함수 내부에서 parse와 rsp의 값을 직접 변경하기 위해 주소 전달
+    _if.R.rdi = count;
+    _if.R.rsi = (char *)_if.rsp + 8;
 
 	/* If load failed, quit. */
 	palloc_free_page (file_name);
@@ -188,6 +212,43 @@ process_exec (void *f_name) {
 	/* Start switched process. */
 	do_iret (&_if);
 	NOT_REACHED ();
+}
+
+void argument_stack(char **parse, int count, void **rsp) // 주소를 전달받았으므로 이중 포인터 사용
+{
+    // 프로그램 이름, 인자 문자열 push
+    for (int i = count - 1; i > -1; i--)
+    {
+        for (int j = strlen(parse[i]); j > -1; j--)
+        {
+            (*rsp)--;                      // 스택 주소 감소
+            **(char **)rsp = parse[i][j]; // 주소에 문자 저장
+        }
+        parse[i] = *(char **)rsp; // parse[i]에 현재 rsp의 값 저장해둠(지금 저장한 인자가 시작하는 주소값)
+    }
+
+    // 정렬 패딩 push
+    int padding = (int)*rsp % 8;
+    for (int i = 0; i < padding; i++)
+    {
+        (*rsp)--;
+        **(uint8_t **)rsp = 0; // rsp 직전까지 값 채움
+    }
+
+    // 인자 문자열 종료를 나타내는 0 push
+    (*rsp) -= 8;
+    **(char ***)rsp = 0; // char* 타입의 0 추가
+
+    // 각 인자 문자열의 주소 push
+    for (int i = count - 1; i > -1; i--)
+    {
+        (*rsp) -= 8; // 다음 주소로 이동
+        **(char ***)rsp = parse[i]; // char* 타입의 주소 추가
+    }
+
+    // return address push
+    (*rsp) -= 8;
+    **(void ***)rsp = 0; // void* 타입의 0 추가
 }
 
 
