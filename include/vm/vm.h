61e1b664c930111a7c419e7ed50577f22e7d86c6
goplayzig <goplayzig@gmail.com>
2024-11-25 20:26:40 +0900

    ~

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -103,7 +103,7 @@ void vm_init (void);
 bool vm_try_handle_fault (struct intr_frame *f, void *addr, bool user,
 		bool write, bool not_present);
 
-#define vm_alloc_page(type, upage, writable) 
+#define vm_alloc_page(type, upage, writable) \
 	vm_alloc_page_with_initializer ((type), (upage), (writable), NULL, NULL)
 bool vm_alloc_page_with_initializer (enum vm_type type, void *upage,
 		bool writable, vm_initializer *init, void *aux);
