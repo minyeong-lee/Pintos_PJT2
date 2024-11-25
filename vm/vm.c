ae1370250911aa32dfeaa0f9faa7e343f8c3b093
goplayzig <goplayzig@gmail.com>
2024-11-25 22:00:22 +0900

    edit code

--- vm/vm.c
+++ vm/vm.c
@@ -229,23 +229,23 @@ vm_do_claim_page (struct page *page) {
 	return swap_in(page, frame->kva);
 }
 
-/* Returns true if page a precedes page b. */
-bool page_less(const struct hash_elem *a_,
-               const struct hash_elem *b_, void *aux UNUSED)
-{
-    const struct page *a = hash_entry(a_, struct page, hash_elem);
-    const struct page *b = hash_entry(b_, struct page, hash_elem);
+// /* Returns true if page a precedes page b. */
+// bool page_less(const struct hash_elem *a_,
+//                const struct hash_elem *b_, void *aux UNUSED)
+// {
+//     const struct page *a = hash_entry(a_, struct page, hash_elem);
+//     const struct page *b = hash_entry(b_, struct page, hash_elem);
 
-    return a->va < b->va;
-}
+//     return a->va < b->va;
+// }
 
-/* Returns a hash value for page p. */
-unsigned
-page_hash(const struct hash_elem *p_, void *aux UNUSED)
-{
-    const struct page *p = hash_entry(p_, struct page, hash_elem);
-    return hash_bytes(&p->va, sizeof p->va);
-}
+// /* Returns a hash value for page p. */
+// unsigned
+// page_hash(const struct hash_elem *p_, void *aux UNUSED)
+// {
+//     const struct page *p = hash_entry(p_, struct page, hash_elem);
+//     return hash_bytes(&p->va, sizeof p->va);
+// }
 
 /* Initialize new supplemental page table */
 void
