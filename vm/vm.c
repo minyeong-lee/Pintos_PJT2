ec168eb985b84cc3ff168dd029865d46b809c103
goplayzig <goplayzig@gmail.com>
2024-11-26 22:46:35 +0900

    edit call hash_insert

--- vm/vm.c
+++ vm/vm.c
@@ -80,7 +80,7 @@ spt_insert_page (struct supplemental_page_table *spt UNUSED,
 		struct page *page UNUSED) {
 	int succ = false;
 	/* TODO: Fill this function. */
-	if hash_insert(&spt, &page->hash_elem) == NULL {
+	if (hash_insert(&spt, &page->hash_elem) == NULL) {
 		succ = true;
 	} else {
 		succ = false;
